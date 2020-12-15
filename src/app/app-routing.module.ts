8777cd62dbe8739a64b36ed815f0abaf72db5cdf
Jesus Nunez <jesusleonardo122@gmail.com>
2020-12-15 12:31:55 -0500

    Inicio del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:PrincipalComponent},
+  {path: 'Personajes', component:PersonajesComponent},
+  {path: '**', pathMatch:'full', redirectTo:'' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
